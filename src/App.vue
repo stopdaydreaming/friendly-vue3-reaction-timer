2842f6f8550a5941999ea458aeb2f53a111a7479
stopdaydreaming <hello@stopdaydreaming.com>
2022-12-22 14:35:27 -0500

    component lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <BlockTimer :delay="delay" v-if="isPlaying"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BlockTimer from './components/BlockTimer.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { BlockTimer },
+  data(){
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+      this.delay = setTimeout(() => {
+        this.isPlaying = true;
+      }, Math.random() * 5000 + 2000);
+      this.isPlaying = true;
+      console.log(this.delay);
+    }
   }
 }
 </script>
@@ -20,7 +34,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
